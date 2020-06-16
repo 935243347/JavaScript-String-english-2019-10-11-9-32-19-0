189575a813c5e0fca05cbc430b79b5fa1d3a13ec
935243347 <35753557+935243347@users.noreply.github.com>
2020-06-16 16:41:15 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
-var name = 'hello';
-// TODO write your code here...
+
+    // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
+    var name = 'hello';
+    // TODO write your code here...
+    console.log(name.toUpperCase());
 
 
-// Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
-var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+    // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
+    var sentence = 'good afternoon, mr mike.';
+    //TODO write your code here...
+    var tmp = sentence.split(' ');
+    for (var i = 0; i < tmp.length; i++){
+        tmp[i] = tmp[i].charAt(0).toUpperCase() + tmp[i].substring(1)
+    }
+    console.log(tmp.join(' '));
 
 
-// Complete the following procedure to extract the amount of money variable, output: 20.
-var money = '￥20';
-//TODO write your code here...
+    // Complete the following procedure to extract the amount of money variable, output: 20.
+    var money = '￥20';
+    //TODO write your code here...
+    console.log(money.substring(1))
+
